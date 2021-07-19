8ce9fec2d5b502199d05a9be8e840e37063d6588
Md. Nahid Hasan Limon <41167616+NahidHasanLimon@users.noreply.github.com>
2021-07-19 23:59:45 +0600

    Currency _format added

--- src/index.php
+++ src/index.php
@@ -10,5 +10,17 @@ class Index
         $returned_string = $greet.$added_string;
         return $returned_string;
     }
+    
+        function currency_format($number){
+            $number=round($number,2);
+        
+        
+        
+            $number = preg_replace("/(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/i", "$1,", $number);
+        
+        
+            return $number;
+            
+        }
 }
 
